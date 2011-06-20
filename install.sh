bbe8696ec8fe969c0de458286394905ab2b70149
Roberson C Ribeiro <robersom@gmail.com>
2011-06-20 00:27:32 -0300

    criado arquivo de instalacao - created install file

---
+++ install.sh
@@ -0,0 +1,49 @@
+#dotfile setup
+#installation file created by roberson
+#diretorio do dotfiles - dotfiles dir
+diretorio="dotfiles"
+
+echo "=========================================="
+echo "instalando dotfiles - installing dotfiles"
+
+cd
+if [ -d "$diretorio" ]; then
+
+  if [ ! -L .bash_aliases ]; then
+    ln -s dotfiles/bash_profile .bash_aliases
+  fi
+
+  if [ ! -L .gemrc ]; then
+    ln -s dotfiles/gemrc .gemrc
+  fi
+
+  if [ ! -L .irbrc ]; then
+    ln -s dotfiles/irbrc .irbrc
+  fi
+
+  if [ ! -L .gitignore ]; then
+    ln -s dotfiles/gitignore .gitignore
+  fi
+
+  if [ ! -d ~/bin ]; then
+    mkdir ~/bin
+  fi
+
+  if [ ! -L bin/ ]; then
+    ln -s dotfiles/git-publish-branch bin/
+  fi
+
+  echo "criados links simbolicos - created symbolic links"
+  echo ""
+  echo "Atualizando bash - renew bash"
+  source ~/.bashrc
+
+else
+
+  echo "diretorio $diretorio nao existe"
+  echo "dir $diretorio dont exists"
+
+fi 
+
+echo "fim da instalacao - End of installation "
+echo "=========================================="
